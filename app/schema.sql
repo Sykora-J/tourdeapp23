8b22983ef5f60ac700d735f91acf391839e18cfe
adamr <adam.rott05@gmail.com>
2023-01-07 19:12:48 +0100

    working db initialize

--- app/schema.sql
+++ app/schema.sql
@@ -1,4 +1,14 @@
-CREATE TABLE IF NOT EXISTS record (
+CREATE TABLE IF NOT EXISTS developer (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   name TEXT NOT NULL
+);
+INSERT INTO developer (name) values ('John');
+CREATE TABLE IF NOT EXISTS devlog (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  work_date TEXT NOT NULL,
+  lang TEXT NOT NULL,
+  duration INTEGER NOT NULL,
+  rating INTEGER NOT NULL,
+  note TEXT,
+  developer_id INTEGER NOT NULL
 );
