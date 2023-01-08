c5649eeb46b1b1c016d0282406f8582acb1f90a1
Jakub Sýkora <docjam17@gmail.com>
2023-01-08 16:17:57 +0100

    added for for dev select

--- app/app.py
+++ app/app.py
@@ -2,6 +2,7 @@ import os
 
 from flask import Flask
 from flask import render_template
+from flask import request
 from . import db
 
 app = Flask(__name__)
@@ -26,8 +27,10 @@ def hello_world():  # put application's code here
 
 @app.route('/list')
 def log_list():  # put application's code here
-    logs = db.select_all_logs()
-    return render_template('log_list.html', logs=logs)
+    for_dev = request.args.get('dev', 'all')
+    logs = db.select_all_logs() # TODO log for only one dev
+    devs = db.select_all_devs()
+    return render_template('log_list.html', logs=logs, devs=devs, for_dev=for_dev)
 
 
 if __name__ == '__main__':
